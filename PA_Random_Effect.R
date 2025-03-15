b4c0845203da7f416a62d26e343ea3995625da03
Bill-Wang-Metrics <wang.13945@osu.edu>
2025-03-15 11:06:07 -0400

    Updated PA_Random_Effect.R with more sophisticated setup

--- PA_Random_Effect.R
+++ PA_Random_Effect.R
@@ -11,23 +11,25 @@ library(gaussquad)
 library(statmod)
 set.seed(42)
 
-u <- function(x,q) 5*x+q-2
-logit <- function(x,q) exp(u(x,q))/(1+exp(u(x,q)))
-Pa <- function(x,q) pnorm(u(x,q))
-# Pa <-  function(x,q) 1
-P10 <- function(x,q) Pa(x,q)*logit(x,q)
-P11 <- function(x,q) logit(x,q)
+u2 <- function(x1,x2,q) 5*x1 - 3*x2 + q - 1
+u1 <- function(x1,x2,q) 3*x1 - 5*x2 + q - 2
+
+logit <- function(x1,x2,q) exp(u2(x1,x2,q))/(1+exp(u2(x1,x2,q)))
+Pa <- function(x1,x2,q) exp(u1(x1,x2,q))/(1+exp(u1(x1,x2,q)))
+P10 <- function(x1,x2,q) Pa(x1,x2,q)*logit(x1,x2,q)
+P11 <- function(x1,x2,q) logit(x1,x2,q)
 
 # observe k households for T months
 T = 24
 k = 300
-x <- runif(k)
+x1 <- runif(k)
+x2 <- rbern(k,.5) - 0.5
 q <- rnorm(k)
-P <- P10(x,q)
+P <- P10(x1,x2,q)
 D0 <- rbern(k,P)
 
 Takeup_generator <- function(D0){
-  prob_takeup <- ifelse(D0==1,P11(x,q),P10(x,q))
+  prob_takeup <- ifelse(D0==1,P11(x1,x2,q),P10(x1,x2,q))
   Dt <- rbern(k,prob_takeup)
 }
 
@@ -76,15 +78,16 @@ plot(x_FA,ylim = c(0,1000),xlab = "x",main = "",col = alpha("red", 0.5),add = TR
 
 q_FA <- hist(FA[,3])
 q_SA <- hist(SA[,3])
-plot(q_SA,ylim = c(0,1000),xlab = "q",main = "",col = alpha("skyblue", 0.75))
-plot(q_FA,ylim = c(0,1000),xlab = "q",main = "",col = alpha("red", 0.5),add = TRUE)
+plot(q_SA,ylim = c(0,1500),xlab = "q",main = "",col = alpha("skyblue", 0.75))
+plot(q_FA,ylim = c(0,1500),xlab = "q",main = "",col = alpha("red", 0.5),add = TRUE)
 
-data_GQ <- data.frame(matrix(0,nrow = k*(T+1), ncol = 5))
-colnames(data_GQ) <- c("id","month","WIC_1","X","q")
+data_GQ <- data.frame(matrix(0,nrow = k*(T+1), ncol = 6))
+colnames(data_GQ) <- c("id","month","WIC_1","X1","X2","q")
 data_GQ$id <- rep(1:k,each = T+1)
 data_GQ$month <- rep(seq(1,T+1),k)
 data_GQ$WIC_1 <- c(Dall)
-data_GQ$X <- rep(x,each = T+1)
+data_GQ$X1 <- rep(x1,each = T+1)
+data_GQ$X2 <- rep(x2,each = T+1)
 data_GQ$q <- rep(q,each = T+1)
 
 data_GQ <- data_GQ %>%
@@ -96,15 +99,28 @@ data_GQ <- data_GQ %>%
   ungroup()
 
 
-utility <- function(Q,a,b,c,x){
-  q <- rep(Q, length.out = length(x))
-  r <- sqrt(2*c^2) * q
-  a * x + r + rep(b, length.out = length(x)) 
+utility <- function(Q,a,b,c,sigma,x){
+  n = dim(x)[1]
+  x1 <- x$X1
+  x2 <- x$X2
+  q <- rep(Q, length.out = n)
+  r <- sqrt(2*sigma^2) * q
+  a * x1 + b * x2 + r + rep(c, length.out = n) 
 }
 
-Palogit <- function(Q,a,b,c,data_GQid){
-  u <- utility(Q,a,b,c,data_GQid$X)
-  Pit <- pnorm(u)^(data_GQid$ID0 == 1) * exp(u)/(1+exp(u))
+ua <- function(Q,d,e,f,sigma,x){
+  n = dim(x)[1]
+  x1 <- x$X1
+  x2 <- x$X2
+  q <- rep(Q, length.out = n)
+  r <- sqrt(2*sigma^2) * q
+  d * x1 + e * x2 + r + rep(f, length.out = n) 
+}
+
+Palogit <- function(Q,a,b,c,d,e,f,sigma,data_GQid){
+  u2 <- utility(Q,a,b,c,sigma,data_GQid)
+  u1 <- ua(Q,d,e,f,sigma,data_GQid)
+  Pit <- (exp(u1)/(1+exp(u1)))^(data_GQid$ID0 == 1) * (exp(u2)/(1+exp(u2)))
   # Pit <- exp(u)/(1+exp(u))
   prod(Pit^(data_GQid$WIC_1 == 1)*(1 - Pit)^(data_GQid$WIC_1 == 0) + 1e-10) 
 } 
@@ -113,24 +129,16 @@ quadrature <- gauss.quad(n = 100, kind = "hermite")
 nodes <- quadrature$nodes
 weights <- quadrature$weights
 
-# Define the integral function
-integrate_Palogit <- function(a, b, c, data_GQid) {
-  # Evaluate the integrand at the quadrature nodes
-  integrand_values <- sapply(nodes, function(Q) {
-    Palogit(Q, a, b, c, data_GQid)
-  })
-  
-  # Approximate the integral using Gaussian quadrature
-  integral <- sum(weights * integrand_values)
-  
-  return(integral)
-}
 
 
 nll <- function(params){
   a <- params[1]
   b <- params[2]
-  c <- exp(params[3])
+  c <- params[3]
+  d <- params[4]
+  e <- params[5]
+  f <- params[6]
+  sigma <- exp(params[7])
   
   neg_ll <- 0
   for(i in 1:k){
@@ -138,7 +146,7 @@ nll <- function(params){
     
     # Evaluate the integrand at the quadrature nodes
     integrand_values <- sapply(nodes, function(Q) {
-      Palogit(Q, a, b, c, data_GQid)
+      Palogit(Q, a, b, c, d, e, f, sigma, data_GQid)
     })
     
     # Approximate the integral using Gaussian quadrature
@@ -149,6 +157,5 @@ nll <- function(params){
   return(neg_ll)
 }
 
-#optim(par = c(2,2,2), fn = nll, method = "L-BFGS-B",lower = c(-Inf,-Inf,1e-10))
-#optim(par = c(5,-2,1), fn = nll, method = "BFGS")
-optim(par = c(2,2,2), fn = nll, method = "BFGS")
+
+optim(par = c(2,2,2,2,2,2,2), fn = nll, method = "BFGS")
